3ea869e39dcbe98f5b4411575b422abe951ace45
Justin Kotalik <jukotali@microsoft.com>
2021-01-25 14:33:22 -0800

    Better check

--- src/BuiltInTools/dotnet-watch/LaunchBrowserFilter.cs
+++ src/BuiltInTools/dotnet-watch/LaunchBrowserFilter.cs
@@ -75,7 +75,7 @@ namespace Microsoft.DotNet.Watcher.Tools
 
                         const string dotnetStartHooksName = "DOTNET_STARTUP_HOOKS";
                         var pathToMiddleware = Path.Combine(AppContext.BaseDirectory, "middleware", "Microsoft.AspNetCore.Watch.BrowserRefresh.dll");
-                        context.ProcessSpec.EnvironmentVariables[dotnetStartHooksName] = AddOrAppend(dotnetStartHooksName, pathToMiddleware, RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? ';' : ':');
+                        context.ProcessSpec.EnvironmentVariables[dotnetStartHooksName] = AddOrAppend(dotnetStartHooksName, pathToMiddleware, Path.PathSeparator);
 
                         const string hostingStartupAssembliesName = "ASPNETCORE_HOSTINGSTARTUPASSEMBLIES";
                         context.ProcessSpec.EnvironmentVariables[hostingStartupAssembliesName] = AddOrAppend(hostingStartupAssembliesName, "Microsoft.AspNetCore.Watch.BrowserRefresh", ';');
